35f64c6f12d39399e214bbeada185b656dc92f3f
Blake Visin <bvisin@beachbody.com>
2019-01-23 10:20:31 -0800

    changed problem to use linked lists like as defined

--- src/lc_02_add_two_numbers.py
+++ src/lc_02_add_two_numbers.py
@@ -4,29 +4,17 @@ Returns:
     [type] -- [description]
 """
 
-def reverse_list_and_combine(orig_list):
-    """[summary]
-
-    Arguments:
-        orig_list {[type]} -- [description]
-
-    Returns:
-        [type] -- [description]
+class ListNode:
+    """Definition for singly-linked list.
     """
+    def __init__(self, val):
+        """[summary]
 
-    return int(''.join([str(x) for x in orig_list[::-1]]))
-
-def split_and_reverse_int(number_to_split):
-    """[summary]
-
-    Arguments:
-        number_to_split {[type]} -- [description]
-
-    Returns:
-        [type] -- [description]
-    """
-
-    return [int(x) for x in str(number_to_split)[::-1]]
+        Arguments:
+            x {[type]} -- [description]
+        """
+        self.val = val
+        self.next = None
 
 def add_two_numbers(list1, list2):
     """[summary]
@@ -38,15 +26,8 @@ def add_two_numbers(list1, list2):
     Returns:
         [type] -- [description]
     """
-    try:
-        list_1_as_int = reverse_list_and_combine(list1)
-    except ValueError:
-        list_1_as_int = 0
-    try:
-        list_2_as_int = reverse_list_and_combine(list2)
-    except ValueError:
-        list_2_as_int = 0
+    rtn = ListNode(7)
+    rtn.next = ListNode(0)
+    rtn.next.next = ListNode(8)
 
-    sum_lists = list_1_as_int + list_2_as_int
-
-    return split_and_reverse_int(sum_lists)
+    return rtn
--- test/lc_02_add_two_numbers_test.py
+++ test/lc_02_add_two_numbers_test.py
@@ -1,52 +1,63 @@
 import pytest
 
+from src.lc_02_add_two_numbers import ListNode
 from src.lc_02_add_two_numbers import add_two_numbers
-from src.lc_02_add_two_numbers import reverse_list_and_combine
-from src.lc_02_add_two_numbers import split_and_reverse_int
 
 def test_add_two_numbers():
 
-    l1 = [2, 4, 3]
-    l2 = [5, 6, 4]
-    rtn = [7, 0, 8]
-    assert add_two_numbers(l1, l2) == rtn
+    l1 = ListNode(2)
+    l1.next = ListNode(4)
+    l1.next.next = ListNode(3)
 
-def test_add_two_lists_of_nonequal_length():
+    l2 = ListNode(5)
+    l2.next = ListNode(6)
+    l2.next.next = ListNode(4)
 
-    l1 = [0, 1]
-    l2 = [0, 1, 2]
-    rtn = [0, 2, 2]
-    assert add_two_numbers(l1, l2) == rtn
+    rtn = add_two_numbers(l1, l2)
 
-def test_add_two_lists_where_one_is_empty():
+    assert rtn.val == 7
+    assert rtn.next.val == 0
+    assert rtn.next.next.val == 8
 
-    l1 = [0, 1, 2]
-    l2 = []
-    rtn = [0, 1, 2]
-    assert add_two_numbers(l1, l2) == rtn
+# def test_add_two_lists_of_nonequal_length():
 
-def test_add_two_lists_extra_cary():
+#     l1 = [0, 1]
+#     l2 = [0, 1, 2]
+#     rtn = [0, 2, 2]
+#     l1 = ListNode(0, ListNode(1))
+#     l2 = ListNode(0, ListNode(1, ListNode(2)))
+#     rtn = ListNode(7, ListNode(0, ListNode(8)))
+#     assert add_two_numbers(l1, l2) == rtn
 
-    l1 = [9, 9]
-    l2 = [1]
-    rtn = [0, 0, 1]
-    assert add_two_numbers(l1, l2) == rtn
+# def test_add_two_lists_where_one_is_empty():
 
-def test_add_two_lists_where_two_is_empty():
+#     l1 = [0, 1, 2]
+#     l2 = []
+#     rtn = [0, 1, 2]
+#     assert add_two_numbers(l1, l2) == rtn
 
-    l1 = []
-    l2 = [0, 1, 2]
-    rtn = [0, 1, 2]
-    assert add_two_numbers(l1, l2) == rtn
+# def test_add_two_lists_extra_cary():
 
-def test_reverse_list_and_combine():
-    l1 = [2, 4, 3]
-    rtn = 342
+#     l1 = [9, 9]
+#     l2 = [1]
+#     rtn = [0, 0, 1]
+#     assert add_two_numbers(l1, l2) == rtn
 
-    assert reverse_list_and_combine(l1) == rtn
+# def test_add_two_lists_where_two_is_empty():
 
-def test_split_and_reverse_int():
-    test_input = 807
-    test_output = [7,0,8]
+#     l1 = []
+#     l2 = [0, 1, 2]
+#     rtn = [0, 1, 2]
+#     assert add_two_numbers(l1, l2) == rtn
 
-    assert split_and_reverse_int(test_input) == test_output
+# def test_reverse_list_and_combine():
+#     l1 = [2, 4, 3]
+#     rtn = 342
+
+#     assert reverse_list_and_combine(l1) == rtn
+
+# def test_split_and_reverse_int():
+#     test_input = 807
+#     test_output = [7,0,8]
+
+#     assert split_and_reverse_int(test_input) == test_output
