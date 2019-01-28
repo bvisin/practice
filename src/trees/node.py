ad89d2962114540a75d401b3bd873d930ddace36
Blake Visin <bvisin@beachbody.com>
2019-01-27 21:14:28 -0800

    Added an insert method to the binary tree node

--- src/trees/node.py
+++ src/trees/node.py
@@ -16,6 +16,27 @@ class Node:
         self.right = None
         self.data = data
 
+    def insert(self, data):
+        """left
+
+        Arguments:
+            data {[type]} -- [description]
+        """
+
+        if data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            if data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
     def print(self):
         """[summary]
         """
--- test/trees/trees_test.py
+++ test/trees/trees_test.py
@@ -7,3 +7,31 @@ def test_node(capsys):
     n.print()
     out,err=capsys.readouterr()
     assert out=="5\n"
+
+def test_insert_node_null():
+    n = Node(5)
+    n.insert(None)
+    assert n.data == None
+
+def test_insert_node_value():
+    n = Node(5)
+    n.insert(10)
+    assert n.data == 5
+    assert n.left == None
+    assert n.right.data == 10
+
+def test_insert_right_node_recursive():
+    root_node = Node(5)
+    assert root_node.data == 5
+    root_node.insert(10)
+    assert root_node.right.data == 10
+    root_node.insert(20)
+    assert root_node.right.right.data == 20
+
+def test_insert_left_node_recursive():
+    root_node = Node(20)
+    assert root_node.data == 20
+    root_node.insert(10)
+    assert root_node.left.data == 10
+    root_node.insert(5)
+    assert root_node.left.left.data == 5
