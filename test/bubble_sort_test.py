232ba530b908412d295fed6ca034a6a808494643
Blake Visin <bvisin@beachbody.com>
2019-02-01 14:50:10 -0800

    renamed  bubble sort files

--- test/bubble_sort_test.py
+++
@@ -1,5 +0,0 @@
-import pytest
-
-def test_bubble_sort():
-
-    array_to_sort = [3,5,7,1,2,3]
---
+++ test/bv_bubble_sort_test.py
@@ -0,0 +1,9 @@
+import pytest
+
+def test_bubble_sort1():
+    unsorted_array = [6,5,4,3,2,1]
+    assert selection_sort(unsorted_array) == [1,2,3,4,5,6]
+
+def test_bubble_sort2():
+    unsorted_array = [55,22,75,24,10,100,5000,48,1,5]
+    assert selection_sort(unsorted_array) == [1,5,10,22,24,48,55,75,100,5000]
