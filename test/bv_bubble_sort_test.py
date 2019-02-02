7fa65fe8f0500e1cefe1921ec336ed87d4b58de0
Blake Visin <bvisin@beachbody.com>
2019-02-01 16:45:16 -0800

    Added the bubble sort algorithm with an early break

--- test/bv_bubble_sort_test.py
+++ test/bv_bubble_sort_test.py
@@ -1,9 +1,22 @@
 import pytest
+from src.bv_bubble_sort import bubble_sort
+
 
 def test_bubble_sort1():
-    unsorted_array = [6,5,4,3,2,1]
-    assert selection_sort(unsorted_array) == [1,2,3,4,5,6]
+    unsorted_array = [6, 5, 4, 3, 2, 1]
+    assert bubble_sort(unsorted_array) == [1, 2, 3, 4, 5, 6]
+
 
 def test_bubble_sort2():
-    unsorted_array = [55,22,75,24,10,100,5000,48,1,5]
-    assert selection_sort(unsorted_array) == [1,5,10,22,24,48,55,75,100,5000]
+    unsorted_array = [55, 22, 75, 24, 10, 100, 5000, 48, 1, 5]
+    assert bubble_sort(unsorted_array) == [
+        1, 5, 10, 22, 24, 48, 55, 75, 100, 5000]
+
+
+def test_bubble_sort_already_sorted_array():
+    unsorted_array = [1, 2, 3, 4, 5]
+    assert bubble_sort(unsorted_array) == [1, 2, 3, 4, 5]
+
+def test_bubble_sort_partially_sorted_array():
+    unsorted_array = [2, 3, 1, 4, 5]
+    assert bubble_sort(unsorted_array) == [1, 2, 3, 4, 5]
